1a053b186428ce6bdd50b43f9067a0eaa2226c3a
Isadora Ávila <isadorgravila@gmail.com>
2022-10-10 16:00:51 -0300

    feat: change API url - prod

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@
 export default function urlAPI(endpoint) {
 	const prod = `https://volleychat.herokuapp.com/${endpoint}`;
 	const test = `http://localhost:5000/${endpoint}`;
-	return test;
+	return prod;
 }
