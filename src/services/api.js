e00ae357b7ff0b9e0b47fd98d8cc4aef5cfdf7a4
Isadora Ávila <isadorgravila@gmail.com>
2023-01-10 12:07:46 -0300

    feat: create environment variable for base api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 export default function urlAPI(endpoint) {
-	const prod = `https://volleychat.herokuapp.com/${endpoint}`;
-	const test = `http://localhost:5000/${endpoint}`;
-	return prod;
+	// eslint-disable-next-line no-undef
+	const baseUrl = process.env.REACT_APP_API_BASE_URL;
+  
+	const url = `${baseUrl}/${endpoint}`;
+	return url;
 }
